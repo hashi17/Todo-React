399dd23794c6d708cf752bc150ec86c23c9c39f8
hashi17 <h.mabo3@gmail.com>
2021-08-06 14:23:56 +0900

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+    console.log("test");
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // 本来[num, faceShowFlag]と書かないとESLintのエラーとなるが、今回num変数のみ注目させたいので設定をdisableさせる
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(^^♪</p>}
+    </>
+  );
+};
+
+export default App;
